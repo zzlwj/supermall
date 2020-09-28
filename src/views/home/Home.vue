727f4c664accac6a5b119699329e9f684a5eee0a
wj l <1296393688@qq.com>
2020-09-28 19:34:31 +0800

    home封装使用及请求多个数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,40 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home"
+  import NavBar from "components/common/navbar/NavBar"
+  import {getHomeMultidata} from "network/home";
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar
+    },
+    data(){
+      return{
+        banners:[],
+        recommends:[]
+      }
+    },
+    created() {
+      //1.请求多个数据
+      getHomeMultidata().then(res=>{
+        //this.result=res;
+        this.banners=res.data.banner.list;
+        this.recommends=res.data.recommend.list;
+      })
     }
+  }
 </script>
 
 <style scoped>
-
+  .home-nav{
+    background-color: var(--color-tint);
+    color:#fff
+  }
 </style>
